a5b8d235bffa670e4afa58beddb54c8aae8f89db
Felipe Menezes <mfelipe758@gmail.com>
2022-12-10 16:58:11 -0300

    commit teste

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import ui.AgendaUI;
 public class Main {
     public static void main(String[] args) {
         AgendaUI angendaUI = new AgendaUI();
-
+        System.out.println("teste bro");
         angendaUI.menuInicial();
     }
 }
